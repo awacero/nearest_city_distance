f437d28d7d820e871387bef9e0545440586cbe76
Jean Aray <80015450+jeanaray13@users.noreply.github.com>
2022-10-13 12:42:13 -0500

    Commented following PEP8 standard

--- nearest_city_distance/app_get_distance.py
+++ nearest_city_distance/app_get_distance.py
@@ -12,21 +12,32 @@ import get_distance
 
 app = Flask(__name__)
 
-##TEMPORAL
+# TEMPORAL
 
 security_token = generate_password_hash("clavemuysecreta")
 
+
 @app.route('/get_nearest_city')
 def call_get_nearest_city():
-    
+
+    """
+    This function returns the name of the country to an event
+
+    Country name is given in ascii.
+
+    :returns: str(get_distance.get_nearest_city(event_location))
+    :rtype: String
+    """
+
     token = request.args.get('token')
-    
+
     if check_password_hash(security_token, token):
         lat = float(request.args.get('lat'))
         lon = float(request.args.get('lon'))
-        event_location = (lat,lon)
+        event_location = (lat, lon)
         print(event_location)
         return str(get_distance.get_nearest_city(event_location))
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
