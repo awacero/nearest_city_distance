a9e92685a53cd1a01865b45a727a30a6ed4215a0
wilson <acerowilson>
2025-09-22 11:09:40 -0500

    add documentation and small changes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN') or  'clavemuysecreta'
-    CITIES_FILE = os.path.join(basedir,'./data/world_cities_RG.csv')
+    CITIES_FILE = os.path.join(basedir,'./data/world_cities_EC.csv')
     COUNTRY_FILE = os.path.join(basedir,'./data/ec_pe_co.shp')
 
     @staticmethod
--- nearest_city_distance/main/get_distance.py
+++ nearest_city_distance/main/get_distance.py
@@ -5,8 +5,6 @@ Created on Aug 5, 2021
 @author: wacero
 '''
 
-
-
 import io
 import numpy as np
 from geopy.distance import distance as geodist
@@ -17,22 +15,32 @@ world_cities_file = config['default'].CITIES_FILE
 
 
 def get_nearest_city(event_location):
-    
+
     """
-    This function return the nearest city and distance to an event. 
-    
-    :param tupla event_location (latitude,longitude)
+    This function return the nearest city and distance to an event.
+    Distance is given in kilometers and city name in ascii.
+
+    :param tupla event_location: (latitude,longitude)
+    :type event_location: tupla
     :returns: tupla: distance in kilometers and city name in ascii. 
     """
+
+
     event_location = np.array([event_location])
-    geo_object = reverse_geocoder.RGeocoder(mode=2, verbose=True, stream=io.StringIO(open(world_cities_file, encoding='utf-8').read()))
+    geo_object = reverse_geocoder.RGeocoder(mode=2, verbose=True,
+                                            stream=io.StringIO(open(world_cities_file,
+                                            encoding='utf-8').read()))
+
     result = geo_object.query(event_location)
     city = result[0]['name']
     province = result[0]['admin1']
-    province = str(unicodedata.normalize('NFKD',province).encode('ascii','ignore'),encoding='utf-8')
+    province = str(unicodedata.
+                   normalize('NFKD',province).
+                   encode('ascii','ignore'),
+                   encoding='utf-8')
     city_location = (result[0]['lat'], result[0]['lon'])
-    distance = geodist(event_location,city_location).kilometers
+    distance = geodist(event_location, city_location).kilometers
 
-    return round(distance,2),city,province
+    return round(distance, 2), city, province
 
 
